429f10b574720b9d0ad69d93b79a83e6a7452701
Nicolás <wantobelieve@gmail.com>
2023-07-06 18:10:17 -0400

    Added and configured sequelizer boilerplate

---
+++ config/config.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+module.exports = {
+  "development": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_DEV,
+    "host": "db",
+    "dialect": "postgres",
+    "port": process.env.DB_PORT
+  },
+  "test": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_TEST,
+    "host": "db",
+    "dialect": "postgres",
+    "port": process.env.DB_PORT
+  },
+  "production": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB,
+    "host": "db",
+    "dialect": "postgres",
+    "port": process.env.DB_PORT
+  }
+};
