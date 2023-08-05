aea6801625dc056120ae9207dc2e6592d7f44964
Nicolás <wantobelieve@gmail.com>
2023-08-04 23:55:27 -0400

    Fix name of `user` object sended to templates

--- controllers/user.js
+++ controllers/user.js
@@ -49,28 +49,28 @@ exports.logout = (req, res, next) => {
 }
 
 exports.getMyAccount = (req, res, next) => {
-  res.render('user/my-account', { userData: req.user, errors: {} });
+  res.render('user/my-account', { user: req.user, errors: {} });
 }
 
 exports.updateMyAccount = async (req, res, next) => {
   const errors = await userValidations.updateMyAccount(req.user.id, req.body);
   if (errors) {
-    res.render('user/my-account', { userData: req.body, errors: errors });
+    res.render('user/my-account', { user: req.body, errors: errors });
   } else {
-    res.render('user/my-account', { userData: req.user, errors: {} });
+    res.render('user/my-account', { user: req.user, errors: {} });
   }
 }
 
 exports.getUpdatePassword = (req, res, next) => {
-  res.render('user/password', { errors: {}, success: false });
+  res.render('user/password', { user: req.user, errors: {}, success: false });
 }
 
 exports.postUpdatePassword = async (req, res, next) => {
   const errors = await userValidations.updatePassword(req.user.id, req.body);
   if (errors) {
-    res.render('user/password', { errors: errors, success: false });
+    res.render('user/password', { user: req.user, errors: errors, success: false });
   } else {
-    res.render('user/password', { errors: {}, success: true });
+    res.render('user/password', { user: req.user, errors: {}, success: true });
   }
 }
 
