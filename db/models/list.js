cfb2a4f356a1f182beeaa7c6148a3633ba55cfba
Nicolás <wantobelieve@gmail.com>
2023-08-06 23:37:03 -0400

    Fixed default value for `position`

--- db/models/list.js
+++ db/models/list.js
@@ -4,7 +4,7 @@ const { Model } = require('sequelize');
 const { dinero, toSnapshot } = require('dinero.js');
 const currencies = require('@dinero.js/currencies');
 
-let listCountOnWorkspace = 0;
+let listCountOnWorkspace;
 
 module.exports = (sequelize, DataTypes) => {
   class List extends Model {
@@ -32,6 +32,7 @@ module.exports = (sequelize, DataTypes) => {
       // });
     }
   }
+
   List.init({
     name: {
       type: DataTypes.STRING,
@@ -45,13 +46,9 @@ module.exports = (sequelize, DataTypes) => {
     position: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      defaultValue: () => {
-        return listCountOnWorkspace + 1;
-      },
       validate: {
         isInt: {
-          // TODO: add explanation note
-          args: { gt: 0, lt: listCountOnWorkspace + 2 },
+          // args: { gt: 0, lt: listCountOnWorkspace + 2 },
           msg: 'El valor ingresado debe ser un número entero.'
         }
       }
@@ -99,14 +96,14 @@ module.exports = (sequelize, DataTypes) => {
     modelName: 'List',
   });
 
-  List.beforeValidate(async () => {
-    const { count } = await this.findAndCountAll({
-      where: {
-        workspaceId: this.workspaceId
-      }
-    });
-    listCountOnWorkspace = count;
-  })
+  // List.beforeValidate(async (list) => {
+  //   const { count } = await List.findAndCountAll({
+  //     where: {
+  //       workspaceId: list.workspaceId
+  //     }
+  //   });
+  //   listCountOnWorkspace = count;
+  // });
 
   return List;
 };
