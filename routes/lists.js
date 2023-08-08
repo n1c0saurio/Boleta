c46a35320f5d6999509d1ed75553db8218882f84
Nicolás <wantobelieve@gmail.com>
2023-08-08 19:29:06 -0400

    Added routes for deletion of Lists and Items

--- routes/lists.js
+++ routes/lists.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const list = require('../controllers/list');
 
 // User dashboard
-router.get('/', list.getList);
-router.post('/', list.postList);
+router.get('/', list.getListsAndItems);
+router.post('/', list.postListOrItem);
+router.get('/:list_id', list.deleteList);
+router.get('/item/:item_id', list.deleteItem);
 
 module.exports = router;
