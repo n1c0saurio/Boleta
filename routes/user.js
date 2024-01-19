fdce23566f1b63d64eefbc9f633870191b6a24b6
Nicolás <wantobelieve@gmail.com>
2024-01-19 15:56:11 -0300

    refactor: delete unused routes

--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,5 @@ const user = require('../controllers/user.js');
 // My account
 router.get('/', user.getMyAccount);
 router.post('/', user.updateMyAccount);
-router.get('/contrasena', user.getUpdatePassword);
-router.post('/contrasena', user.postUpdatePassword);
 
 module.exports = router;
